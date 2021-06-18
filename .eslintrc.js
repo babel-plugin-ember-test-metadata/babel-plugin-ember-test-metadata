f99c30a72fec44a920cb1867bdc4824fad7fd639
Glenn Cueto <gcueto@linkedin.com>
2021-06-18 16:24:26 -0700

    address feedback

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:node/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 12,
