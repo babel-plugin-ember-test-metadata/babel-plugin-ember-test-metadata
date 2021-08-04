73ef767254deb75d8a26fe2c7bba0f6f8b21cffe
Glenn Cueto <gcueto@linkedin.com>
2021-08-03 17:54:37 -0700

    remove console log

--- src/index.js
+++ src/index.js
@@ -203,8 +203,6 @@ function shouldLoadFile(filename) {
  * @returns Babel plugin object with Program and CallExpression visitors
  */
 function addMetadata({ types: t }) {
-  console.log('env var', process.env.BABEL_TEST_METADATA);
-
   if (!process.env.BABEL_TEST_METADATA) {
     return {};
   }
